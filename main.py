c21850b1ef62cb0ee774d87a8c7b10f904ed6331
Rene Kjellerup <rk.katana.steel@gmail.com>
2020-01-13 18:04:51 -0800

    added function to save a message
    added form to input a promo message
    created promo id generator

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 # import frbddn
 import wine_metric
+import promo
 
 
 def getAccinfo(d):
@@ -15,6 +16,13 @@ def getAccinfo(d):
     if 'lic' in d:
         a = d['lic']
     return (a, d['googleAcc'])
+
+
+def getStaticData(fname):
+    res = open(appdir + fname, 'r')
+    resbody = res.read()
+    res.close()
+    return resbody
 
 
 @app.route('/store_user.php', methods=['GET', 'POST'])
@@ -28,11 +36,19 @@ def store_user():
     wine_metric.store_user(id, lic)
     return ''
 
-def getStaticData(fname):
-    res = open(appdir + fname, 'r')
-    resbody = res.read()
-    res.close()
-    return resbody
+
+@app.route('/promo_create', methods=['GET', 'POST'])
+def promo_setup():
+    if request.method == 'GET':
+        if promo.client is not None:
+            return getStaticData('/static/create_promo.html')
+        else:
+            return make_response('', 403)
+    else:
+        (title, msg) = (request.args['title'], request.args['msg']
+        linkId = promo.store_message(title, msg)
+        a = '<a href="/?promo={0}">promo id: {0}</a>'.format(linkId)
+        return a
 
 
 @app.route('/robot.txt', methods=['GET'])
@@ -76,4 +92,3 @@ def Root():
     if 'link' in request.args:
         return links(request.args['link'])
     return getStaticData('/static/main.html')
-
--- promo.py
+++ promo.py
@@ -1,4 +1,5 @@
 from google.google.cloud import ndb
+from hashlib import sha224
 
 client = ndb.Client()
 
@@ -7,8 +8,32 @@ class Message(ndb.Model):
     promoID = ndb.StringProperty(indexed=True)
     title = ndb.StringProperty(indexed=False)
     msg = ndb.StringProperty(indexed=False)
-    created = ndb.DateTimeProperty(auto_now=True,indexed=True)
+    created = ndb.DateTimeProperty(auto_now=True, indexed=True)
+
+
+def generate_id(title):
+    check = sha224(title.encode())
+    genid = sha224(check.digest())
+    check.update(genid.digest())
+    genid.update(check.digest())
+    check.update(genid.digest())
+    genid.update(check.digest())
+    return genid.hexdigest()[0:16]
+
+
+def store_message(title, msg):
+    with client.context():
+        m = Message()
+        m.title = title
+        m.msg = msg
+        m.promoID = generate_id(title)
+        m.put()
 
 
 def get_message_by_id(idx):
-    pass
+    with client.context():
+        try:
+            m = Message.query(Message.promoID == idx)
+            return (m[0].title, m[0].msg)
+        except Exception:
+            return None
